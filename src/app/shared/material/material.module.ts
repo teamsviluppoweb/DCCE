e4d0b4a221bd9ba147c22001fa83857987181532
Robert <robertturturica@gmail.com>
2019-12-23 00:03:24 +0100

    chore: Aggiunto modulo flex layout

---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,73 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatDialogModule, MatStepperModule, MatTableModule} from '@angular/material/';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import {LayoutModule} from '@angular/cdk/layout';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatDividerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatStepperModule,
+    MatTableModule,
+    MatDialogModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatDividerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatStepperModule,
+    MatTableModule,
+    MatDialogModule,
+  ]
+})
+export class MaterialModule { }
